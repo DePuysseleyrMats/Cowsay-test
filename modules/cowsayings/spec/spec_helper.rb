d7aa5d4e6d49146d89e154f93a78cb948e18af69
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-13 10:13:55 +0100

    Update spec_helper.rb

--- modules/cowsayings/spec/spec_helper.rb
+++ modules/cowsayings/spec/spec_helper.rb
@@ -1,6 +1,6 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
-require 'rspec-puppet'
-require 'rspec-puppet/spec_helper'
+require '../../puppetlabs_spec_helper/module_spec_helper'
+require '../../rspec-puppet'
+require '../../rspec-puppet/spec_helper'
 
 
 RSpec.configure do |c|
