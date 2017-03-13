a513ee033a41b4df0fbaf34d2580f532b54a4c2b
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-13 11:59:36 +0100

    Update init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
-describe 'cowsay' do
-	  it { is_expected.to have_resource_count(1) }
-	  it { is_expected.to have_resource_count(100) }
-end
-
+describe 'init' do
+    it { should contain_class('init') }
+    it { is_expected.to compile }
+    it { is_expected.to have_resource_count(1) }
+    it { is_expected.to contain_package('ntp') }
+  end
