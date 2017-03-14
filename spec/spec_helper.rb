e3e8b0abdea54ee21964ea8993156c1a95e8f819
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-14 12:58:02 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet'
 require 'rspec-puppet/spec_helper'
-
+require 'yaml'
+require 'psych'
 
 RSpec.configure do |c|
   c.module_path = './manifests'
