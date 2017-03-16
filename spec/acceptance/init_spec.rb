c987625d1f97e40718a5ccad1c372e82e60e12c7
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-16 14:46:09 +0100

    Update init_spec.rb

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper_acceptance'
 
 describe 'init' do
   pp = <<-EOS
+    class { 'init': }
+  EOS
+
   apply_manifest(pp, :catch_failures => true)
   expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+
   describe package('ntp') do
     it { is_expected.to be_installed }
   end
