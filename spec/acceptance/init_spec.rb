6722b984ee4338df1017d93feca814c77fb76f2c
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-16 13:50:14 +0100

    Update init_spec.rb

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,21 +1,7 @@
 require 'spec_helper_acceptance'
 
 describe 'init' do
-  case fact('osfamily')
-  when 'RedHat'
-    package_name = 'ntp'
-    service_name = 'ntp'
-  when 'Debian'
-    package_name = 'ntp'
-    service_name = 'ntp'
-  when 'FreeBSD'
-    package_name = 'ntp'
-    service_name = 'ntp'
-  when 'Gentoo'
-    package_name = 'ntp'
-    service_name = 'ntp'
-  when 'Ubuntu'
-    package_name = 'ntp'
-    service_name = 'ntp'
+  it 'should apply without errors' do
+    apply_manifest(manifest, :catch_failures => true)
   end
 end
