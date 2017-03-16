2d1ce31848839fc6947384d6c158dc648c2d1c62
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-16 12:18:45 +0100

    Update spec_helper_acceptance.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,10 +1,6 @@
 # consul/spec/spec_helper_acceptance.rb
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
-require 'docker'
-require 'docker-api'
 
 
 # Not needed for this example as our docker files have puppet installed already
