264ec953d419e77138a1a7b1afee40ac68d75665
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-20 08:39:11 +0100

    Update init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
-describe 'init' do
-    it { should contain_class('init') }
+describe 'Cowsay-test' do
+    it { should contain_class('Cowsay-test') }
     it { is_expected.to compile }
     it { is_expected.to have_resource_count(1) }
     it { is_expected.to contain_package('ntp') }
