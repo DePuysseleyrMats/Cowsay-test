947d7d5c122ece08dc87bfb7a9d09cc6104dd6a1
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-20 12:54:33 +0100

    Update init_spec.rb

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper_acceptance'
 describe 'cowsaytest' do
   let(:manifest) {
     <<-EOS
-      include ::cowsaytest
+      include cowsaytest
     EOS
   }
   it 'should apply without errors' do
