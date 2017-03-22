88178b505e53b5b0306c09e6fe375f17252aa237
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-22 08:33:01 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,9 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet'
 require 'rspec-puppet/spec_helper'
 
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
 RSpec.configure do |c|
-  c.manifest.dir = './manifests'
-
-
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
 end
