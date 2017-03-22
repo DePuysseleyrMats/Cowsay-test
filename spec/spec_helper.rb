63db74259615afec899214a853088afb64d32bbb
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-22 08:21:06 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'rspec-puppet'
 require 'rspec-puppet/spec_helper'
 
 RSpec.configure do |c|
-  c.module_path = './manifests'
-  c.manifest_dir = './manifests'
+  c.module_path = '../../manifests'
+  c.manifest_dir = '../../manifests'
 
 end
