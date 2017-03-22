4cb4fe623248f30b25fc49f459b882cdc959005b
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-22 14:20:59 +0100

    Update spec_helper_acceptance.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -12,10 +12,6 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
   # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..')) 
-  c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
-      
   # Readable test descriptions
   c.formatter = :documentation
 
