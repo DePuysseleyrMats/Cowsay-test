a6d976c981875b556b1ff0574f5066ea09a3c47e
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-22 13:34:01 +0100

    Update init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
-describe 'cowsaytest::cowsaytest' do
-    it { should contain_class('cowsaytest::cowsaytest') }
+describe 'cowsaytest' do
+    it { should contain_class('cowsaytest') }
     it { is_expected.to compile }
     it { is_expected.to have_resource_count(1) }
     ## it { is_expected.to contain_package('vim') }
